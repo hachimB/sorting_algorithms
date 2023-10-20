d51d04cff40e70bf586cfd25d8b62d26938e893b
hachimB <boubacarhachim@gmail.com>
2023-10-20 08:54:58 +0100

    ok

---
+++ H/2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * selection_sort -  sorts an array of integers in ascending order.
+ * @array: array
+ * @size: size
+ * Return: nothing.
+ */
+void selection_sort(int *array, size_t size)
+{
+
+}
--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
-#ifndef sort
-#define sort
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,14 +13,16 @@
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
