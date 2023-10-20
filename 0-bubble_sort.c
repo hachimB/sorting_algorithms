7eaced2f4261742e8af172fef00f1c76bce28de1
hachimB <boubacarhachim@gmail.com>
2023-10-20 11:38:36 +0100

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,12 +10,12 @@ void bubble_sort(int *array, size_t size)
 size_t i, j;
 int flag;
 int temp;
-if (size <= 1 || array == NULL)
+if (!size || !array)
 return;
 for (i = 0; i < size - 1; i++)
 {
-if (array[i] != (int)array[i])
-return;
+/*if (array[i] != (int)array[i])*/
+/*return;*/
 flag = 0;
 for (j = 0; j < size - 1 - i; j++)
 {
