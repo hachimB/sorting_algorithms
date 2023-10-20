3ce79c002f2b6d69dcf29f6d3cf0449ad8693a4e
hachimB <boubacarhachim@gmail.com>
2023-10-20 11:29:09 +0100

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,10 @@ int flag;
 int temp;
 if (size <= 1 || array == NULL)
 return;
-/*if (*array)*/
-/*{*/
 for (i = 0; i < size - 1; i++)
 {
+if (array[i] != (int)array[i])
+return;
 flag = 0;
 for (j = 0; j < size - 1 - i; j++)
 {
@@ -31,5 +31,4 @@ print_array(array, size);
 if (flag == 0)
 break;
 }
-/*}*/
 }
